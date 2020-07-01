48b9468d7dde3decb64e668392a170e9411021b5
Leonardo Augusto Lima Ferreira <index.xp@gmail.com>
2020-07-01 16:55:06 -0300

    Add project files.

---
+++ ApiManagementPOC/Controllers/PurchasesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiManagementPOC.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class PurchasesController : ControllerBase
+    {
+        [HttpGet("/{userId}/[controller]/{purchaseId}")]
+        public IActionResult GetPurchase(int userId, int purchaseId)
+        {
+            return Ok($"User {userId} and Purcase {purchaseId}");
+        }
+
+        [HttpPut("/{userId}/[controller]/{itemId}")]
+        public IActionResult Buy(int userId, int itemId)
+        {
+            return Ok($"User {userId} bought {itemId}");
+        }
+    }
+}
---
+++ ApiManagementPOC/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiManagementPOC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUser(int id)
+        {
+            return Ok("User ID was " + id);
+        }
+
+        [HttpGet("/{id}/details")]
+        public IActionResult GetUserDetails(int id)
+        {
+            return Ok("Detailed ID " + id);
+        }
+
+        [HttpPut]
+        public IActionResult CreateUser(User user)
+        {
+            return Ok("User Created was: " + JsonConvert.SerializeObject(user));
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
